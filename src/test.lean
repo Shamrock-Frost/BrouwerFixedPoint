7ca49cf40b91c4f55d55d8dee049ee36152be5c4
Brendan Murphy <shamrockfrost@gmail.com>
2022-07-02 20:45:59 -0700

    initial commit

---
+++ src/test.lean
@@ -0,0 +1,11 @@
+import algebraic_topology.alternating_face_map_complex
+import algebraic_topology.simplicial_set
+import algebra.category.Module.abelian
+import algebra.category.Module.adjunctions
+import algebra.homology.homology
+import topology.homotopy.basic
+import algebraic_topology.simplex_category
+import algebraic_topology.topological_simplex
+import topology.constructions
+
+example : 0 = 0 := by trivial
